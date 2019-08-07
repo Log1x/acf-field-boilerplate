892a0b1869e043023c95c26fcd5ffbe02766a607
Brandon <brandon@tendency.me>
2019-08-07 00:06:34 -0500

    v1.0.7

--- plugin.php
+++ plugin.php
@@ -1,12 +1,12 @@
 <?php
-/*
-Plugin Name: Advanced Custom Fields: Field Boilerplate
-Plugin URI:  https://github.com/log1x/acf-field-boilerplate
-Description: A better way to create ACF Fields.
-Version:     1.0.5
-Author:      Brandon Nifong
-Author URI:  https://log1x.com
-*/
+/**
+ * Plugin Name: Advanced Custom Fields: Field Boilerplate
+ * Plugin URI:  https://github.com/log1x/acf-field-boilerplate
+ * Description: A better way to create ACF Fields.
+ * Version:     1.0.7
+ * Author:      Brandon Nifong
+ * Author URI:  https://log1x.com
+ */
 
 namespace Log1x\ExampleField;
 
